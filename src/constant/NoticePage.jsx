4abc8b3f9a380cecd72fc60a8e642419fd710251
KwonSuyeon <commetoi_yeoni@naver.com>
2023-01-30 16:58:11 +0900

    noticePage ui 구현

---
+++ src/constant/NoticePage.jsx
@@ -0,0 +1,73 @@
+import * as S from "./NoticePage.style";
+
+const NoticePage = () => {
+  return (
+    <S.Wrapper>
+      <S.Title>NoticePage</S.Title>
+      <S.CardSection>
+        <S.Card>
+          <S.CardTitle>Study1</S.CardTitle>
+          <S.CardSubtitle>CardSubtitle</S.CardSubtitle>
+          <S.CardDescription>
+            Card Description BlaBlaBla Card Description BlaBlaBla Card
+            Description BlaBlaBla Card Description BlaBlaBla Card Description
+            BlaBlaBla Card Description BlaBlaBla Card Description BlaBlaBla Card
+            Description BlaBlaBla Card Description BlaBlaBla Card Description
+            BlaBlaBla Card Description BlaBlaBla Card Description BlaBlaBla
+          </S.CardDescription>
+          <S.CardButton>Click</S.CardButton>
+        </S.Card>
+        <S.Card>
+          <S.CardTitle>Study2</S.CardTitle>
+          <S.CardSubtitle>CardSubtitle</S.CardSubtitle>
+          <S.CardDescription>
+            Card Description BlaBlaBla Card Description BlaBlaBla Card
+            Description BlaBlaBla Card Description BlaBlaBla Card Description
+            BlaBlaBla Card Description BlaBlaBla Card Description BlaBlaBla Card
+            Description BlaBlaBla Card Description BlaBlaBla Card Description
+            BlaBlaBla Card Description BlaBlaBla Card Description BlaBlaBla
+          </S.CardDescription>
+          <S.CardButton>Click</S.CardButton>
+        </S.Card>
+        <S.Card>
+          <S.CardTitle>Study3</S.CardTitle>
+          <S.CardSubtitle>CardSubtitle</S.CardSubtitle>
+          <S.CardDescription>
+            Card Description BlaBlaBla Card Description BlaBlaBla Card
+            Description BlaBlaBla Card Description BlaBlaBla Card Description
+            BlaBlaBla Card Description BlaBlaBla Card Description BlaBlaBla Card
+            Description BlaBlaBla Card Description BlaBlaBla Card Description
+            BlaBlaBla Card Description BlaBlaBla Card Description BlaBlaBla
+          </S.CardDescription>
+          <S.CardButton>Click</S.CardButton>
+        </S.Card>
+        <S.Card>
+          <S.CardTitle>Study4</S.CardTitle>
+          <S.CardSubtitle>CardSubtitle</S.CardSubtitle>
+          <S.CardDescription>
+            Card Description BlaBlaBla Card Description BlaBlaBla Card
+            Description BlaBlaBla Card Description BlaBlaBla Card Description
+            BlaBlaBla Card Description BlaBlaBla Card Description BlaBlaBla Card
+            Description BlaBlaBla Card Description BlaBlaBla Card Description
+            BlaBlaBla Card Description BlaBlaBla Card Description BlaBlaBla
+          </S.CardDescription>
+          <S.CardButton>Click</S.CardButton>
+        </S.Card>
+        <S.Card>
+          <S.CardTitle>Study5</S.CardTitle>
+          <S.CardSubtitle>CardSubtitle</S.CardSubtitle>
+          <S.CardDescription>
+            Card Description BlaBlaBla Card Description BlaBlaBla Card
+            Description BlaBlaBla Card Description BlaBlaBla Card Description
+            BlaBlaBla Card Description BlaBlaBla Card Description BlaBlaBla Card
+            Description BlaBlaBla Card Description BlaBlaBla Card Description
+            BlaBlaBla Card Description BlaBlaBla Card Description BlaBlaBla
+          </S.CardDescription>
+          <S.CardButton>Click</S.CardButton>
+        </S.Card>
+      </S.CardSection>
+    </S.Wrapper>
+  );
+};
+
+export default NoticePage;
---
+++ src/constant/NoticePage.style.js
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1920px;
+`;
+
+export const Title = styled.h1`
+  color: pink;
+  background-color: grey;
+`;
+
+export const CardSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 100px;
+  gap: 20px;
+`;
+
+export const Card = styled.div`
+  display: flex;
+  background-color: grey;
+  /* max-width: 30px; */
+  border-radius: 10px;
+  min-width: 350px;
+  height: 500px;
+  flex-direction: column;
+`;
+
+export const CardTitle = styled.div`
+  color: pink;
+  font-size: 40px;
+  margin-top: 10px;
+`;
+
+export const CardSubtitle = styled.div`
+  color: pink;
+  font-size: 30px;
+  margin-top: 10px;
+`;
+
+export const CardDescription = styled.div`
+  color: pink;
+  font-size: 18px;
+  margin-top: 70px;
+  padding-left: 5px;
+  padding-right: 5px;
+  text-align: center;
+`;
+
+export const CardButton = styled.button`
+  width: 100px;
+  height: 50px;
+  border-radius: 10px;
+  border-color: transparent;
+  background-color: pink;
+  justify-content: flex-end;
+  margin-top: 30px;
+  align-self: center;
+  color: grey;
+
+  &:hover {
+    background-color: lightpink;
+  }
+`;
