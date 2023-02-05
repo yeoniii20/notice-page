8ed84d2712a982e99a57468ce9d7cc5671e246c4
KwonSuyeon <commetoi_yeoni@naver.com>
2023-02-06 02:24:57 +0900

    Header 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./constant/Header";
 import NoticeDetailPage from "./constant/NoticeDetailPage";
 import NoticePage from "./constant/NoticePage";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <NoticePage />
       <NoticeDetailPage />
     </div>
