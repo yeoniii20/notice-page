9586e506d9f35ef7b3a021ca35a9efc99333e073
KwonSuyeon <commetoi_yeoni@naver.com>
2023-02-06 02:32:54 +0900

    noticeDetatilPage ui 수정

--- src/constant/NoticeDetailPage.style.js
+++ src/constant/NoticeDetailPage.style.js
@@ -2,10 +2,11 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  max-width: 1920px;
-  margin-top: 200px;
-  margin-bottom: 300px;
+  background-color: lightgray;
+  padding-top: 200px;
+  padding-bottom: 100px;
+  /* flex-direction: column; */
+  /* max-width: 1920px; */
 `;
 
 export const Title = styled.h1`
@@ -17,7 +18,6 @@ export const CardSection = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
-  margin-top: 100px;
   gap: 20px;
   justify-content: flex-start;
   padding-left: 32px;
@@ -48,9 +48,8 @@ export const CardSubtitle = styled.h2`
 export const CardDescription = styled.div`
   color: pink;
   font-size: 18px;
-  margin-top: 70px;
-  padding-left: 5px;
-  padding-right: 5px;
+  padding-left: 10px;
+  padding-right: 10px;
   text-align: center;
 `;
 
